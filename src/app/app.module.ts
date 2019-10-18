3d745348aef9c6ed664ec7802f545bac51a82c7a
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-17 22:55:15 -0500

    Se importan los compoentes al proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ConversationComponent } from './components/conversation/conversation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    ProfileComponent,
+    ConversationComponent
   ],
   imports: [
     BrowserModule

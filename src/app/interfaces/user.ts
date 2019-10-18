5b074e3a54675f3e6dea86c912b7fa000353da74
DLI <sistemas@dli.com.co>
2019-10-18 17:53:09 -0500

    Se cambia propiedad a opcional

--- src/app/interfaces/user.ts
+++ src/app/interfaces/user.ts
@@ -3,7 +3,7 @@ export interface User {
   nick: string;
   subnick?: string;// Usando el símbolo "?" se peude dejar opcional el campo
   age?: number;// Usando el símbolo "?" se peude dejar opcional el campo
-  status: string;
+  status?: string;
   email: string;
   friend: boolean;
 }

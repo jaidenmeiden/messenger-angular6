62b9719b11b6e5b8b5094607ed767986891aa981
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-17 23:15:37 -0500

    Se crean las primeras rutas del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+// Se importan componentes
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { ConversationComponent } from './components/conversation/conversation.component';
+import { MenuComponent } from './components/menu/menu.component';
+
+// Rutas
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'conversation', component: ConversationComponent},
+  {path: 'profile', component: ProfileComponent}
+];
 
 @NgModule({
   declarations: [
@@ -13,10 +25,12 @@ import { ConversationComponent } from './components/conversation/conversation.co
     HomeComponent,
     LoginComponent,
     ProfileComponent,
-    ConversationComponent
+    ConversationComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]

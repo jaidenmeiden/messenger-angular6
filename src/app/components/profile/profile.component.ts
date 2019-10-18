f1808ac6928284778edea1b722e89f050dad4bc3
DLI <sistemas@dli.com.co>
2019-10-18 17:53:28 -0500

    Se termina profile

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthenticationService} from "../../services/authentication.service";
+import {UserFirebaseService} from "../../services/user-firebase.service";
+import {User} from "../../interfaces/user";
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  user: User;
 
-  constructor() { }
+  constructor(
+    private userFirebaseService: UserFirebaseService,
+    private authenticationService: AuthenticationService
+  ) {
+    this.authenticationService.getStatus().subscribe((status) => {
+      this.userFirebaseService.getUserById(status.uid).valueChanges().subscribe((data: User) => {
+        this.user = data;
+        console.log(this.user);
+      }, (error) => {
+        console.log(error);
+      });
+    }, (error) => {
+      console.log(error);
+    });
+  }
 
   ngOnInit() {
   }
 
+  saveSettings() {
+    this.userFirebaseService.editUser(this.user).then(() => {
+      alert('Cambios guardados!');
+    }).catch((error) => {
+      alert('Hubo un error');
+      console.log(error);
+    });
+  }
 }

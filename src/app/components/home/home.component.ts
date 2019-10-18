e8efbd998e8f3bd4ea3cbd26e068b8e89af6f050
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-17 23:51:12 -0500

    Se crean y se imprimen usuarios de prueba con la interfaz User

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../../interfaces/user';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  private users: User[];
+
+  constructor() {
+    this.users = [
+      {uid: 1, nick: 'Eduardo', subnick: 'Mi mensaje personal', status: 'online', age: 28, email: 'eduardo@platzi.com', friend: true},
+      {uid: 2, nick: 'Yuliana', subnick: 'Mi mensaje personal', status: 'busy', age: 25, email: 'yuliana@platzi.com', friend: true},
+      {uid: 3, nick: 'Freddy', subnick: 'Mi mensaje personal', status: 'away', age: 28, email: 'freddy@platzi.com', friend: false},
+      {uid: 4, nick: 'Jaiden', subnick: 'Mi mensaje personal', status: 'away', age: 32, email: 'jaiden@platzi.com', friend: false},
+      {uid: 5, nick: 'Alfredo', subnick: 'Mi mensaje personal', status: 'away', age: 38, email: 'alfredo@platzi.com', friend: true}
+    ];
+  }
 
   ngOnInit() {
   }

3c9cb08a100671cc9458ad77df347b284880d281
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-18 06:28:13 -0500

    Se implementa servicio User en lista de usuarios

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+
+// Interfaces
 import {User} from '../../interfaces/user';
+
+// Servicios
+import {UserService} from '../../services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -10,14 +15,10 @@ export class HomeComponent implements OnInit {
 
   private users: User[];
 
-  constructor() {
-    this.users = [
-      {uid: 1, nick: 'Eduardo', subnick: 'Mi mensaje personal', status: 'online', age: 28, email: 'eduardo@platzi.com', friend: true},
-      {uid: 2, nick: 'Yuliana', subnick: 'Mi mensaje personal', status: 'busy', age: 25, email: 'yuliana@platzi.com', friend: true},
-      {uid: 3, nick: 'Freddy', subnick: 'Mi mensaje personal', status: 'away', age: 28, email: 'freddy@platzi.com', friend: false},
-      {uid: 4, nick: 'Jaiden', subnick: 'Mi mensaje personal', status: 'away', age: 32, email: 'jaiden@platzi.com', friend: false},
-      {uid: 5, nick: 'Alfredo', subnick: 'Mi mensaje personal', status: 'away', age: 38, email: 'alfredo@platzi.com', friend: true}
-    ];
+  constructor(
+    private userService: UserService
+  ) {
+    this.users = this.userService.getUsers();
   }
 
   ngOnInit() {

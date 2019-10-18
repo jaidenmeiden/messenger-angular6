7fab6c9325ffe90e13167d82d7db4ede3c40391d
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-18 07:15:02 -0500

    Se crea el pipe personalizado 'search'

---
+++ src/app/pipes/search.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'search'
+})
+export class SearchPipe implements PipeTransform {
+
+  public transform(value: any, args: string): any {
+    if (!value) {
+      return null;
+    }
+    if (!args) {
+      return value;
+    }
+    args = args.toLowerCase();
+    return value.filter((item) => {
+      return JSON.stringify(item).toLowerCase().includes(args);
+    });
+  }
+
+}

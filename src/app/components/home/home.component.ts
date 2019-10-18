175588db1336ce8b8ce00715c44e9c50db31c221
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-18 08:02:52 -0500

    Se modifica visualmente el componente home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 // Interfaces
 import {User} from '../../interfaces/user';
 
 // Servicios
 import {UserService} from '../../services/user.service';
+import {AuthenticationService} from '../../services/authentication.service';
 
 @Component({
   selector: 'app-home',
@@ -15,14 +17,29 @@ export class HomeComponent implements OnInit {
 
   private users: User[];
   private query: string;
+  private content: string;
 
   constructor(
-    private userService: UserService
+    public router: Router,
+    private userService: UserService,
+    private authenticationService: AuthenticationService
   ) {
     this.users = this.userService.getUsers();
   }
 
   ngOnInit() {
+  }
+
+  setUserProperty(property, status) {
+
+  }
+  open(contenido) {
+
+  }
+  logOut() {
+    /*this.authenticationService.logOut().then(() => {
+      this.router.navigate(['login']);
+    });*/
   }
 
 }

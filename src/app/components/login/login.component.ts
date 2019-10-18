27f34656437dd8c4972e477d12200c65c0478f09
DLI <sistemas@dli.com.co>
2019-10-18 14:49:21 -0500

    Se implementa login y registro

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthenticationService} from "../../services/authentication.service";
+import {error} from "util";
 
 @Component({
   selector: 'app-login',
@@ -7,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   operation: string = 'login';
+  email: string = null;
+  password: string = null;
+  nick: string = null;
 
-  constructor() { }
+  constructor(
+    private authenticationService: AuthenticationService
+  ) { }
 
   ngOnInit() {
+  }
+
+  login() {
+    this.authenticationService.loginWithEmail(this.email, this.password)
+      .then((data) => {
+        alert('Loggueado correctamente!');
+        console.log(data);
+      }).catch((error) => {
+        alert('Ocurrio un error!');
+        console.log(error);
+      });
+  }
+
+  register() {
+    this.authenticationService.registerWithEmail(this.email, this.password)
+      .then((data) => {
+        alert('Registrado correctamente!');
+        console.log(data);
+      }).catch((error) => {
+      alert('Ocurrio un error!');
+      console.log(error);
+    });
   }
 
 }

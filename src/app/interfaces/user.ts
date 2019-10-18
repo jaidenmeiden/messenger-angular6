fc471760db2e2aeb408920b6646374887c8dee52
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-17 23:50:32 -0500

    Se crea la interfaz User

---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,9 @@
+export interface User {
+  uid: any;
+  nick: string;
+  subnick?: string;// Usando el símbolo "?" se peude dejar opcional el campo
+  age?: number;// Usando el símbolo "?" se peude dejar opcional el campo
+  status: string;
+  email: string;
+  friend: boolean;
+}

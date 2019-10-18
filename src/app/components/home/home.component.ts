a96e4195ba09be453d6bb04f78be878e18d8093c
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-18 07:15:59 -0500

    Se implementa el pipe 'search' en home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -14,6 +14,7 @@ import {UserService} from '../../services/user.service';
 export class HomeComponent implements OnInit {
 
   private users: User[];
+  private query: string;
 
   constructor(
     private userService: UserService

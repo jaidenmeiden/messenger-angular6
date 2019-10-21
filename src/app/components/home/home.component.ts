b65b20655901db0eb6fe81317548075f03e46a1f
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-21 15:12:13 -0500

    Se crea servicio request para proceso de agregar amigos y se crea la infraestructura en el home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 // Interfaces
 import {User} from '../../interfaces/user';
@@ -7,6 +8,7 @@ import {User} from '../../interfaces/user';
 // Servicios
 import {AuthenticationService} from '../../services/authentication.service';
 import {UserFirebaseService} from "../../services/user-firebase.service";
+import {RequestsService} from '../../services/requests.service';
 
 @Component({
   selector: 'app-home',
@@ -15,21 +17,29 @@ import {UserFirebaseService} from "../../services/user-firebase.service";
 })
 export class HomeComponent implements OnInit {
 
-  private users: User[];
-  private query: string;
-  private content: string;
+  private friends: User[];
+  private query: string = '';
+  private friendEmail: string = '';
+  private user: User;
 
   constructor(
     public router: Router,
     private userFirebaseService: UserFirebaseService,
-    private authenticationService: AuthenticationService
+    private authenticationService: AuthenticationService,
+    private modalService: NgbModal,
+    private requestsService: RequestsService
   ) {
     this.userFirebaseService.getUsers().valueChanges()
       .subscribe((data: User[]) => {
-        this.users = data;
+        this.friends = data;
       }, (error) => {
         console.log(error);
       });
+    this.authenticationService.getStatus().subscribe((status) => {
+      this.userFirebaseService.getUserById(status.uid).valueChanges().subscribe((data: User) => {
+        this.user = data;
+      });
+    });
   }
 
   ngOnInit() {
@@ -38,12 +48,29 @@ export class HomeComponent implements OnInit {
   setUserProperty(property, status) {
 
   }
-  open(contenido) {
-
-  }
   logOut() {
     this.authenticationService.logOut().then(() => {
       this.router.navigate(['login']);
+    });
+  }
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+    }, (reason) => {
+    });
+  }
+  sendRequest() {
+    const request = {
+      timestamp: Date.now(),
+      receiver_email: this.friendEmail,
+      sender: this.user.uid,
+      status: 'pending'
+    };
+    this.requestsService.createRequest(request).then(() => {
+      alert('Solicitud Enviada');
+    }).catch((error) => {
+      alert('Hubo un error');
+      console.log(error);
     });
   }
 

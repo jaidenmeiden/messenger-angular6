0484215511bca41057f4e5734c0b5b73ac016deb
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-10-21 17:14:24 -0500

    Se hace implementación de apertura de díalogo de invitación a amigos al cargar el chat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 import {Router} from '@angular/router';
+import {DialogService} from 'ng2-bootstrap-modal';
+import {AuthenticationService} from './services/authentication.service';
+import {UserService} from './services/user.service';
+import {RequestsService} from './services/requests.service';
+import {User} from './interfaces/user';
+import {RequestComponent} from "./components/modals/request/request.component";
+import {UserFirebaseService} from "./services/user-firebase.service";
 
 @Component({
   selector: 'app-root',
@@ -8,8 +15,37 @@ import {Router} from '@angular/router';
 })
 export class AppComponent {
   title = 'Platzinger';
+  user: User;
+  requests: any[] = [];
+  mailsShown: any[] = [];
 
   constructor(
-    public router: Router
-  ) {}
+    public router: Router,
+    private authenticationService: AuthenticationService,
+    private userFirebaseService: UserFirebaseService,
+    private requestService: RequestsService,
+    private dialogService: DialogService
+  ) {
+    this.authenticationService.getStatus().subscribe((status) => {
+      if(status != null) {
+        this.userFirebaseService.getUserById(status.uid).valueChanges().subscribe( (data: User) => {// Se obtine el usuario en la sesión abierta
+          this.user = data;
+          this.requestService.getRequestsForEmail(this.user.email).valueChanges().subscribe((requests: any) => {// Se llaman a los request que hayan llegado
+            this.requests = requests;
+            this.requests = this.requests.filter( (r) => {//Se filtran los request que no hayan sido aceptadas o rechazadas
+              return r.status !== 'accepted' && r.status !== 'rejected';
+            });
+            this.requests.forEach((request) => {// Recorrer las solicitudes validas
+              if (this.mailsShown.indexOf(request.sender) === -1) {
+                this.mailsShown.push((request.sender));
+                this.dialogService.addDialog(RequestComponent, {scope: this, currentRequest: request});
+              }
+            });
+          }, (error) => {
+            console.log(error);
+          });
+        });
+      }
+    });
+  }
 }
